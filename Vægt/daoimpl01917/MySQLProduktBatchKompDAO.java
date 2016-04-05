13fd8853b4d892868c6497f3fe18b7a8954cf7b1
Søren Fritzbøger <Bente@Srens-MacBook-Pro.local>
2016-04-05 13:59:13 +0200

    Added ProduktBatch update, get and insert

--- Vægt/daoimpl01917/MySQLProduktBatchKompDAO.java
+++ Vægt/daoimpl01917/MySQLProduktBatchKompDAO.java
@@ -2,6 +2,8 @@ package daoimpl01917;
 
 import daointerfaces01917.ProduktBatchKompDAO;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import connector01917.Connector;
@@ -13,36 +15,43 @@ public class MySQLProduktBatchKompDAO implements ProduktBatchKompDAO {
 	@Override
 	public ProduktBatchKompDTO getProduktBatchKomp(int pbId, int rbId) throws DALException
 	{
-		// TODO Auto-generated method stub
-		return null;
+		ResultSet rs = Connector.doQuery("Select * FROM produktbatchkomp WHERE pbId = " + pbId + "AND rbId = " + rbId);
+		try {
+			if (!rs.first()) throw new DALException("Produktbatchkomp " + pbId + " findes ikke");
+			return new ProduktBatchKompDTO (rs.getInt("pbId"), rs.getInt("rbId"), rs.getInt("tara"), rs.getInt("netto"),rs.getInt("opr_id"));
+		} catch (SQLException e) {throw new DALException(e); }
 	}
 
 	@Override
 	public List<ProduktBatchKompDTO> getProduktBatchKompList(int pbId) throws DALException
 	{
-		// TODO Auto-generated method stub
-		return null;
+		
 	}
 
 	@Override
 	public List<ProduktBatchKompDTO> getProduktBatchKompList() throws DALException
 	{
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void createProduktBatchKomp(ProduktBatchKompDTO produktbatchkomponent) throws DALException
-	{
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void updateProduktBatchKomp(ProduktBatchKompDTO produktbatchkomponent) throws DALException
+	public void createProduktBatchKomp(ProduktBatchKompDTO pbKomp) throws DALException
 	{
-		// TODO Auto-generated method stub
-		
+		Connector.doUpdate(
+				"INSERT INTO produktbatchkomp(pbId, rbId, tara, netto, opr_id) VALUES " + 
+				"(" + pbKomp.getPbId() +", '" + pbKomp.getRbId() + "', '" + pbKomp.getTara() + "', '" + 
+				pbKomp.getNetto() + "', '" + pbKomp.getOprId() + "')"
+				);
+	}
+
+	@Override
+	public void updateProduktBatchKomp(ProduktBatchKompDTO pbKomp) throws DALException
+	{
+		Connector.doUpdate(
+				"UPDATE produktbatchkomp set tara = '" + pbKomp.getTara() + "', netto = '" + pbKomp.getNetto() + 
+				"', opr_id = '" + pbKomp.getOprId() + "' WHERE pbId = " + pbKomp.getPbId() + 
+				" AND rbId = " + pbKomp.getRbId()
+				);
 	}
 
 }
