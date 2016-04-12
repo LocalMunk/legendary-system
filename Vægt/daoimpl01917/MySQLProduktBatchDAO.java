f6b44c1a6ac970d83149eb74d54553e06235f806
TheGundel <rasmusgundel@hotmail.dk>
2016-04-12 09:30:58 +0200

    Added methods

--- Vægt/daoimpl01917/MySQLProduktBatchDAO.java
+++ Vægt/daoimpl01917/MySQLProduktBatchDAO.java
@@ -2,6 +2,8 @@ package daoimpl01917;
 
 import daointerfaces01917.ProduktBatchDAO;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import connector01917.Connector;
@@ -13,29 +15,52 @@ public class MySQLProduktBatchDAO implements ProduktBatchDAO{
 	@Override
 	public ProduktBatchDTO getProduktBatch(int pbId) throws DALException
 	{
-		// TODO Auto-generated method stub
-		return null;
+		ResultSet rs = Connector.doQuery("Select pbId, status, receptId FROM produktBatch WHERE pbId = ?", pbId);
+		try {
+			if (!rs.first())
+				throw new DALException("Produktbatchen" + pbId + "findes ikke");
+		
+			return new ProduktBatchDTO (rs.getInt("pbId"), rs.getInt("status"), rs.getInt("receptId"));
+		} 
+		catch (SQLException e)
+		{
+			throw new DALException(e);
+		}
 	}
 
 	@Override
 	public List<ProduktBatchDTO> getProduktBatchList() throws DALException
 	{
-		// TODO Auto-generated method stub
-		return null;
+		List<ProduktBatchDTO> list = new ArrayList<ProduktBatchDTO>();
+		ResultSet rs = Connector.doQuery("SELECT pbId, status, receptId FROM produktbatch");
+		try
+		{
+			while (rs.next())
+			{
+				list.add(new ProduktBatchDTO(rs.getInt("pbId"), rs.getInt("status"), rs.getInt("receptId")));
+			}
+		}
+		catch (SQLException e) { throw new DALException(e); }
+		return list;
 	}
 
 	@Override
 	public void createProduktBatch(ProduktBatchDTO produktbatch) throws DALException
 	{
-		// TODO Auto-generated method stub
-		
+		Connector.doQuery
+		(
+			"INSERT INTO produktBatch(pbId, status, receptId) VALUES (?,?,?)",
+				produktbatch.pbId, produktbatch.status, produktbatch.receptId
+				);
 	}
 
 	@Override
 	public void updateProduktBatch(ProduktBatchDTO produktbatch) throws DALException
 	{
-		// TODO Auto-generated method stub
-		
+		Connector.doQuery(
+				"UPDATE produktbatch SET status = ?,  receptId = ? WHERE pbId = ?",
+				produktbatch.status, produktbatch.receptId, produktbatch.pbId
+				);
 	}
 	
 	
