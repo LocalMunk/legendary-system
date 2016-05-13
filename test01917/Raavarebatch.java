da4e460fe9157408eb401502f1e04f5b1fb49aec
Kristian Wiwe <wiwe123@hotmail.com>
2016-05-13 11:05:28 +0200

    Added RaavareBatch test

---
+++ test01917/Raavarebatch.java
@@ -0,0 +1,58 @@
+package test01917;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import connector01917.Connector;
+import connector01917.Constant;
+import daoimpl01917.MySQLOperatoerDAO;
+import daoimpl01917.MySQLRaavareBatchDAO;
+import daointerfaces01917.DALException;
+import dto01917.OperatoerDTO;
+import dto01917.RaavareBatchDTO;
+
+public class Raavarebatch {
+
+Connector db;
+	
+	@Before
+	public void setup() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+		db = new Connector(Constant.server, Constant.port, Constant.database, Constant.username, Constant.password);
+	}
+
+	@Test
+	public void GetRaavare() throws DALException {
+		MySQLRaavareBatchDAO  conn = new MySQLRaavareBatchDAO();
+		
+		RaavareBatchDTO op = conn.getRaavareBatch(23);
+		assertNotNull(op);
+	}
+
+	@Test
+	public void GetRaavareBatchList() throws DALException {
+		MySQLRaavareBatchDAO  conn = new MySQLRaavareBatchDAO();
+		List<RaavareBatchDTO> op = conn.getRaavareBatchList();
+		assertNotNull(op);
+	}
+	
+	@Test
+	public void UpdateRaavareBatch() throws DALException {
+		RaavareBatchDTO x = new RaavareBatchDTO(23, 233, 3);
+		MySQLRaavareBatchDAO conn = new MySQLRaavareBatchDAO();
+		conn.updateRaavareBatch(x);
+		
+	}
+
+	@Test
+	public void CreateOperator() throws DALException {
+		RaavareBatchDTO x = new RaavareBatchDTO(23, 40, 94);
+		MySQLRaavareBatchDAO conn = new MySQLRaavareBatchDAO();
+		conn.createRaavareBatch(x);
+		
+}
+}
